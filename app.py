b906c5c4f801b49327194249c7f19f917e7bb5d4
Sumer Malhotra <sumermalhotra1998@gmail.com>
2021-12-09 20:49:17 +0800

    Make API queryable

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from utils import *
 from fastapi import FastAPI
 
-alert(f"{os.getenv('NAME')}: Starting server for {os.getenv('OPENSEA_SLUG')}")
+alert(f"{os.getenv('NAME')}: Starting server for Opensea")
 app = FastAPI()
 logger = setup_custom_logger('root')
-setup_file_logger(os.getenv("OPENSEA_SLUG"), logger)
+setup_file_logger('opensea', logger)
 
-@app.get(f"/floor/{os.getenv('OPENSEA_SLUG')}")
-async def root():
+@app.get(f"/floor")
+async def root(slug: str):
     try:
-        floor = opensea.get_floor(os.getenv('OPENSEA_SLUG'))
+        floor = opensea.get_floor(slug)
         return {'result': 1, 'data': {'price': floor}}
     except:
-        alert(f"{os.getenv('NAME')}: Some bug for {os.getenv('OPENSEA_SLUG')}")
+        alert(f"{os.getenv('NAME')}: Some bug for {slug}")
         return {'result': 0, 'data': {'error': 'Opensea API bug'}}
